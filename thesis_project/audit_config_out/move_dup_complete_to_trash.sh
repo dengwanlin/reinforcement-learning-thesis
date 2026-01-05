7356e86bb05907d7902db3d976047a5bd2597f67
dengwanlin <whhxsg@gmail.com>
2026-01-05 02:26:07 +0000

    update after cleansing

--- thesis_project/audit_config_out/move_dup_complete_to_trash.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-mkdir -p '/homes/sohawan2/reinforcement-learning-thesis/thesis_project/runs_seed0__trash'
-mkdir -p '/homes/sohawan2/reinforcement-learning-thesis/thesis_project/runs_seed0__trash/CartPole-v1/a2c'
-mv '/homes/sohawan2/reinforcement-learning-thesis/thesis_project/runs_seed0/CartPole-v1/a2c/20251026_041229_350542_pid801056_seed0' '/homes/sohawan2/reinforcement-learning-thesis/thesis_project/runs_seed0__trash/CartPole-v1/a2c/20251026_041229_350542_pid801056_seed0'
--- thesis_project/audit_config_out/move_dup_incomplete_to_trash.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-mkdir -p '/homes/sohawan2/reinforcement-learning-thesis/thesis_project/runs_seed0__trash'
